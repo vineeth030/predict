f281ece31cf032a15a9814719784ab01be174553
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2024-06-06 11:53:08 +0530

    stars_collected

--- app/Http/Controllers/Api/PointController.php
+++ app/Http/Controllers/Api/PointController.php
@@ -160,49 +160,54 @@ class PointController extends Controller
     public function allUserPoints()
     {
         $companyGroupId = auth()->user()->company_group_id;
-    
+
         $users = User::leftJoin('points', 'users.id', '=', 'points.user_id')
-            ->select('users.id', 'users.image', 'users.fav_team', 'users.name',
+                 ->leftJoin('cards_game', 'users.id', '=', 'cards_game.user_id')
+            ->select(
+                'users.id',
+                'users.image',
+                'users.fav_team',
+                'users.name',
                 DB::raw('COALESCE(SUM(points.points), 0) as total_points'),
                 DB::raw('CAST(COALESCE(users.old_rank, 0) AS UNSIGNED) as old_rank'),
-                DB::raw('CAST(COALESCE(users.new_rank, 0) AS UNSIGNED) as new_rank'))
+                DB::raw('CAST(COALESCE(users.new_rank, 0) AS UNSIGNED) as new_rank'),
+                DB::raw('IFNULL(LENGTH(cards_game.cards_opened) - LENGTH(REPLACE(cards_game.cards_opened, ",", "")) + 1, 0) as stars_collected')
+
+            )
             ->where('users.company_group_id', $companyGroupId)
             ->where('users.verified', 1)
             ->groupBy('users.id', 'users.name', 'users.image', 'users.old_rank', 'users.new_rank', 'users.fav_team')
             ->orderBy('total_points', 'desc')
             ->orderBy('name', 'asc')
             ->get();
-    
+
         // Initialize rank
         $rank = 1;
         $baseImagePath = url('storage/profile_images/');
-    
+
         foreach ($users as $user) {
             $userModel = User::find($user->id);
-            
+
             // Update old and new ranks
             $user->old_rank = $userModel->new_rank;
             $user->new_rank = $rank;
-            
+
             // Calculate rank change
             $rankChange = $user->new_rank - $user->old_rank;
             $user->rank_change = $rankChange > 0 ? '+1' : ($rankChange < 0 ? '-1' : '0');
-            
+
             // Update image path
             $user->image = $user->image ? $baseImagePath . '/' . $user->image : null;
-            
+
             // Save updated rank in the User model
             $userModel->new_rank = $rank;
             $userModel->save();
             $user->new_rank =  $userModel->new_rank;
             $user->old_rank = (int) $userModel->old_rank;
-            
+
             $rank++;
         }
-    
+
         return response()->json(['status' => 200, 'message' => 'success', 'data' => $users]);
     }
-    
-
-
 }

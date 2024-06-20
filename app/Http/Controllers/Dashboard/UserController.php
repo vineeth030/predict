90a3d0a5ee71e7051daab61ab3e464e8f09a97ef
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2024-06-20 10:21:21 +0530

    view_pagination

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 
     public function index(Request $request)
     {
-        $users = User::paginate(10); // 10 users per page
+        $users = User::paginate(50); // 10 users per page
         return view('users', compact('users'));
     }
 }

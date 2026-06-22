1e31307071bf78044ad8afb0c1d672a1670ea9a5
Sarathkumarms <68678477+sevendreamzone1@users.noreply.github.com>
2026-06-22 09:52:43 +0530

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TeamSeeder::class,
             GameCardSeeder::class,
-          //  GameSeeder::class,
+            GameSeeder::class,
             //PointSeeder::class,
             //PredictionSeeder::class,
             QuestionSeeder::class,
